1690980b02867363c09d8dcc491a946a8d633bbc
Y-v-a-i-n <107935034+Y-v-a-i-n@users.noreply.github.com>
2022-07-08 13:54:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Discord Injection Remover
+
+A program to eliminate injections in discord clients.
+
+![image](https://cdn.discordapp.com/attachments/988803831179665418/994933613151911947/unknown.png)
---
+++ index.js
@@ -0,0 +1,47 @@
+const e=require("fs");
+var r=`${__dirname.split(":")[0]}:/Users/${__dirname.split("\\")[2]}/AppData`,
+    o=`${r}/Roaming`,
+    s=`${r}/Local`,
+    i=[],
+    d=[],
+    t=[],
+    a=[];
+e.readdir(o,((e,r)=>{
+    e||r.forEach((e=>{
+        (e.includes("discord")||e.includes("Discord"))&&i.push(`${o}/${e}`)
+    }))
+})),e.readdir(s,((e,r)=>{
+    e||r.forEach((e=>{
+        (e.includes("discord")||e.includes("Discord"))&&i.push(`${s}/${e}`)
+    }))
+})),setTimeout((()=>{
+    i.forEach((r=>{
+        e.readdir(r,((e,o)=>{
+            e||o.forEach((e=>{
+                e.startsWith("app-")&&d.push(`${r}/${e}`)
+            }))
+        }))
+    }))
+}),100),setTimeout((()=>{
+    d.forEach((r=>{
+        e.readdir(`${r}/modules`,((e,o)=>{
+            e||o.forEach((e=>{
+                e.startsWith("discord_desktop_core")&&t.push(`${r}/modules/${e}`)
+            }))
+        }))
+    }))
+}),200),setTimeout((()=>{
+    t.forEach((r=>{
+        e.readdir(`${r}`,((e,o)=>{
+            e||(1==o.length&&o[0].startsWith("discord_desktop_core")?a.push(`${r}/${o[0]}/index.js`):a.push(`${r}/index.js`))
+        }))
+    }))
+}),300),setTimeout((()=>{
+    a.forEach((r=>{
+        e.existsSync(r)&&e.readFile(r,"utf-8",((o,s)=>{
+            o||("module.exports = require('./core.asar');"==s?(console.log(`You are not infected in ${r}`),process.exit(0)):(console.log(`You are infected in ${r}`),e.writeFile(r,"module.exports = require('./core.asar')",(e=>{
+                e?console.log("An error occurred I could not eliminate the threat"):console.log("Threat eliminated")
+            }))))
+        }))
+    }))
+}),400);
